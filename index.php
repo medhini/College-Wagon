dcadcafbcb0bef4135f5d054f31f06c88aef9354
Anant Maheshwari <anantm95@gmail.com>
2014-10-03 20:10:34 +0530

    Create index.php
    Home Page for website.

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>College Wagon</title>
+<link href="site.css" rel="stylesheet">
+</head>
+<body>
+<nav id="nav01"></nav>
+<div id="main">
+<?php 
+echo '<h1>Welcome to College Wagon!</h1>';
+echo '<p> The one stop destination for buying and selling second hand goods at the go. <p>'; 
+echo '<h2>Sell or Advertise anything&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="sell.php">Post Free Ad</a></h2>'; 
+?> 
+ 
+<footer id="foot01"></footer>
+</div>
+<script src="script.js"></script>
+</body>
+</html>
