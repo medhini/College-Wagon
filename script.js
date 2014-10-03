70d263f88d0b25ad74d00763757f813c137b5b29
Anant Maheshwari <anantm95@gmail.com>
2014-10-03 20:11:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " College Wagon.</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.php'>Home</a></li>" +
+"<li><a href='buy.php'>Buy</a></li>" +
+"<li><a href='about.php'>About</a></li>" +
+"<li><a href='contact.php'>Contact Us</a></li>"+
+"</ul>";
