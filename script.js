f7b80379a77c1f6cf66f7c2a27bd36390ab5cb8a
Anant Maheshwari <anantm95@gmail.com>
2014-12-27 15:28:31 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " College Wagon.</p>";
-document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='index.php'>Home</a></li>" +
-"<li><a href='buy.php'>Buy</a></li>" +
-"<li><a href='about.php'>About</a></li>" +
-"<li><a href='contact.php'>Contact Us</a></li>"+
-"</ul>";
